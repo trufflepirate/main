1ddca90400206b255f7411e50dd13391b9f9831b
Teo Jun Jie <e0148239@u.nus.edu>
2018-09-19 23:23:41 +0800

    Changed stuff according to travis error

--- src/main/java/seedu/address/model/TimeStamp.java
+++ src/main/java/seedu/address/model/TimeStamp.java
@@ -2,6 +2,11 @@ package seedu.address.model;
 
 import java.util.Date;
 
+
+/**
+ * Represents a TimeStamp Object for Machines.
+ */
+
 public class TimeStamp {
 
     private final Date createdTimeStamp;
@@ -10,16 +15,16 @@ public class TimeStamp {
         this.createdTimeStamp = new Date(System.currentTimeMillis());
     }
 
-    private long getTime(){
+    private long getTime() {
         return createdTimeStamp.getTime();
     }
 
 
-    private long getDate(){
+    private long getDate() {
         return createdTimeStamp.getTime();
     }
 
-    private long getElapsedTime(){
+    private long getElapsedTime() {
         return System.nanoTime() - createdTimeStamp.getTime();
     }
 
