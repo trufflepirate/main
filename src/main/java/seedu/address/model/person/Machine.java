09ff5aab29c58abe6f9b29de5d9665995b808bd3
trufflepirate <rootfourtytwo@gmail.com>
2018-09-19 22:33:14 +0800

    Even more style violations!

--- src/main/java/seedu/address/model/person/Machine.java
+++ src/main/java/seedu/address/model/person/Machine.java
@@ -2,12 +2,12 @@ package seedu.address.model.person;
 
 import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Objects;
 import java.util.Set;
-import java.util.List;
-import java.util.ArrayList;
 
 import seedu.address.model.tag.Tag;
 
