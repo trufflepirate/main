8530c90ebded271def163553381df8173258425d
Psyf <gzsaif001@gmail.com>
2018-09-19 17:58:22 +0800

    Added Admin class and component classes

---
+++ src/main/java/seedu/address/model/admin/Admin.java
@@ -0,0 +1,17 @@
+package seedu.address.model.admin;
+
+/**
+ * Represents an Admin in printQ9000.
+ */
+public class Admin {
+    private Username username;
+    private PasswordHash passwordHash;
+
+    public Admin (Username username, PasswordHash passwordHash) {
+        this.username = username;
+        this.passwordHash = passwordHash;
+    }
+
+    public Username getUsername() { return username; }
+    public PasswordHash getPasswordHash() { return passwordHash; }
+}
---
+++ src/main/java/seedu/address/model/admin/Username.java
@@ -0,0 +1,15 @@
+package seedu.address.model.admin;
+
+/**
+ * Represents the Username of an admin
+ */
+public class Username {
+
+    private String value;
+
+    public Username(String username) {
+        this.value = username;
+    }
+
+    public String toString() { return value; }
+}
