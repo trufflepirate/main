6e1d6f5db72e253e5a736be708a317f78be982c5
Teo Jun Jie <e0148239@u.nus.edu>
2018-09-19 19:25:36 +0800

    Added timestamp class havent integrated with the rest of the code

---
+++ src/main/java/seedu/address/model/TimeStamp.java
@@ -0,0 +1,25 @@
+package seedu.address.model;
+
+import java.util.Date;
+
+public class TimeStamp {
+
+    private final Date createdTimeStamp;
+
+    public TimeStamp() {
+        this.createdTimeStamp = new Date(System.currentTimeMillis());
+    }
+
+    private long getTime(){
+        return createdTimeStamp.getTime();
+    }
+
+    private long getDate(){
+        return createdTimeStamp.getTime();
+    }
+
+    private long getElapsedTime(){
+        return System.nanoTime() - createdTimeStamp.getTime();
+    }
+    
+}
