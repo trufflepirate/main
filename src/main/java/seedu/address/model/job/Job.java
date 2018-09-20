d6ca482baebb97f76e83443f5b26541ecb489a15
Psyf <gzsaif001@gmail.com>
2018-09-20 17:42:54 +0800

    change name and machine placeholders to classes in Job class

--- src/main/java/seedu/address/model/job/Job.java
+++ src/main/java/seedu/address/model/job/Job.java
@@ -1,23 +1,26 @@
 package seedu.address.model.job;
 
+import seedu.address.model.person.Machine;
+import seedu.address.model.person.Name;
+
 /**
  * Represents a Printing job in printQ9000
  */
 public class Job {
-    private String name;
-    private String machine;
+    private Name name;
+    private Machine machine;
     TimeStamp time = new TimeStamp();
 
-    public Job(String name, String machine) {
-        this.name = name;
+    public Job(String name, Machine machine) {
+        this.name = new Name(name);
         this.machine = machine;
     }
 
-    public String getName() {
+    public Name getName() {
         return name;
     }
 
-    public String getMachine() {
+    public Machine getMachine() {
         return machine;
     }
 
@@ -26,10 +29,10 @@ public class Job {
     }
 
     public void changeName(String name) {
-        this.name = name;
+        this.name = new Name(name);
     }
 
-    public void changeMachine(String machine) {
+    public void changeMachine(Machine machine) {
         this.machine = machine;
     }
 
