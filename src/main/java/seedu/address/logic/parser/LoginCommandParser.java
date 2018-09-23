e4f31967addda174176b5c318066a1228f019701
Psyf <gzsaif001@gmail.com>
2018-09-23 23:20:22 +0800

    move to proper classes in LoginCommand

--- src/main/java/seedu/address/logic/parser/LoginCommandParser.java
+++ src/main/java/seedu/address/logic/parser/LoginCommandParser.java
@@ -3,6 +3,8 @@ package seedu.address.logic.parser;
 
 import seedu.address.logic.commands.LoginCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.admin.PasswordHash;
+import seedu.address.model.admin.Username;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
@@ -15,8 +17,8 @@ public class LoginCommandParser implements Parser<LoginCommand> {
         }
 
         String [] temp = trimmedArgs.split(" ");
-        String username = temp[0];
-        String password = temp[1];
+        Username username = new Username(temp[0]);
+        PasswordHash password = new PasswordHash(temp[1]);
 
         return new LoginCommand(username, password);
     }
