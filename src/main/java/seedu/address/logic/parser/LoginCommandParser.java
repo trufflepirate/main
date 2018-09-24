42d1f5815bbfa9955130cbc1748db318384de40b
Psyf <gzsaif001@gmail.com>
2018-09-24 10:52:46 +0800

    Rename PasswordHash to Password; Add to LoginCommandTest

--- src/main/java/seedu/address/logic/parser/LoginCommandParser.java
+++ src/main/java/seedu/address/logic/parser/LoginCommandParser.java
@@ -3,7 +3,7 @@ package seedu.address.logic.parser;
 
 import seedu.address.logic.commands.LoginCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
-import seedu.address.model.admin.PasswordHash;
+import seedu.address.model.admin.Password;
 import seedu.address.model.admin.Username;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
@@ -18,7 +18,7 @@ public class LoginCommandParser implements Parser<LoginCommand> {
 
         String [] temp = trimmedArgs.split(" ");
         Username username = new Username(temp[0]);
-        PasswordHash password = new PasswordHash(temp[1]);
+        Password password = new Password(temp[1]);
 
         return new LoginCommand(username, password);
     }
--- src/test/java/seedu/address/logic/commands/LoginCommandTest.java
+++ src/test/java/seedu/address/logic/commands/LoginCommandTest.java
@@ -4,11 +4,16 @@ import seedu.address.logic.CommandHistory;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
+import seedu.address.model.admin.Password;
+import seedu.address.model.admin.Username;
 import seedu.address.ui.testutil.EventsCollectorRule;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class LoginCommandTest {
     @Rule
@@ -24,4 +29,32 @@ public class LoginCommandTest {
     public void execute_login_throwsCommandException() throws Exception {
         thrown.expect(CommandException.class);
     }
+
+    @Test
+    public void equals() {
+        Username bob = new Username("bob");
+        Username alice = new Username("alice");
+        Password alicePw = new Password("123456");
+        Password bobPw = new Password("abcdef");
+
+        LoginCommand LoginAliceCommand = new LoginCommand(alice, alicePw);
+        LoginCommand LoginBobCommand = new LoginCommand(bob, bobPw);
+
+        // same object -> returns true
+        assertTrue(LoginAliceCommand.equals(LoginAliceCommand));
+
+        // same values -> returns true
+        LoginCommand LoginAliceCommandCopy = new LoginCommand(alice, alicePw);
+        assertTrue(LoginAliceCommand.equals(LoginAliceCommandCopy));
+
+        // different types -> returns false
+        assertFalse(LoginAliceCommand.equals(1));
+
+        // null -> returns false
+        assertFalse(LoginAliceCommand.equals(null));
+
+        // different person -> returns false
+        assertFalse(LoginAliceCommand.equals(LoginBobCommand));
+    }
+
 }
