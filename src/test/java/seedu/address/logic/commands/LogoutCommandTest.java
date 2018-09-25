73567e3f705893bcd2fba8633ab28c64bc4c75ad
Psyf <gzsaif001@gmail.com>
2018-09-25 21:11:24 +0800

    Style check...again

--- src/test/java/seedu/address/logic/commands/LogoutCommandTest.java
+++ src/test/java/seedu/address/logic/commands/LogoutCommandTest.java
@@ -1,25 +1,28 @@
 package seedu.address.logic.commands;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
 import seedu.address.logic.CommandHistory;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.ui.testutil.EventsCollectorRule;
 
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.rules.ExpectedException;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-
 public class LogoutCommandTest {
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
     @Rule
     public final EventsCollectorRule eventsCollectorRule = new EventsCollectorRule();
 
     private Model model = new ModelManager();
     private Model expectedModel = new ModelManager();
     private CommandHistory commandHistory = new CommandHistory();
-
-    public ExpectedException thrown = ExpectedException.none();
 
     @Test
     public void equals() {
