866c0ff14e55145a813ed071f9b938d6258e0820
Psyf <gzsaif001@gmail.com>
2018-09-25 14:09:20 +0800

    Add remove method in UniqueAdminList

---
+++ src/main/java/seedu/address/model/admin/exceptions/AdminNotFoundException.java
@@ -0,0 +1,6 @@
+package seedu.address.model.admin.exceptions;
+
+/**
+ * Signals that the operation is unable to find the specified admin.
+ */
+public class AdminNotFoundException extends RuntimeException {}
