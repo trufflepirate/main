44ad9e2be74bc98ede7894a141314be13ed2a827
Psyf <gzsaif001@gmail.com>
2018-09-25 16:18:49 +0800

    Make changes to LogoutCommand#execute

--- src/main/java/seedu/address/logic/commands/LogoutCommand.java
+++ src/main/java/seedu/address/logic/commands/LogoutCommand.java
@@ -3,20 +3,28 @@ package seedu.address.logic.commands;
 import seedu.address.logic.CommandHistory;
 import seedu.address.logic.commands.Command;
 import seedu.address.logic.commands.CommandResult;
+import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 
 public class LogoutCommand extends Command {
 
     public static final String COMMAND_WORD = "logout";
-    public static final String MESSAGE_SUCCESS = "logout success!";
-    public static final String MESSAGE_FAILURE = "logout failed!";
+    public static final String MESSAGE_SUCCESS = "logout successful!";
+    private static final String MESSAGE_NO_CURRENT_SESSION = "Logout failed! No one is currently logged in.";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + "Logout used to get out of admin mode.\n"
             + "Example: logout\n";
 
 
+
     @Override
-    public CommandResult execute(Model model, CommandHistory history) {
+    public CommandResult execute(Model model, CommandHistory history) throws CommandException {
+        if (!model.isLoggedIn()) {
+            throw new CommandException(MESSAGE_NO_CURRENT_SESSION);
+        }
+        model.clearLogin();
+        model.commitAddressBook(); //TODO: Not sure what this does
+
         return new CommandResult(MESSAGE_SUCCESS);
     }
 
