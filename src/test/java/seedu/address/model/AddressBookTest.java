400b8f0ee573b4913b5b7ccf1d3f199b92ec7ac7
Psyf <gzsaif001@gmail.com>
2018-09-29 14:09:21 +0800

    Fix issues in tests

--- src/test/java/seedu/address/model/AddressBookTest.java
+++ src/test/java/seedu/address/model/AddressBookTest.java
@@ -19,6 +19,7 @@ import org.junit.rules.ExpectedException;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import seedu.address.model.admin.Admin;
 import seedu.address.model.machine.Machine;
 import seedu.address.model.person.Person;
 import seedu.address.model.person.exceptions.DuplicatePersonException;
@@ -97,6 +98,7 @@ public class AddressBookTest {
      */
     private static class AddressBookStub implements ReadOnlyAddressBook {
         private final ObservableList<Person> persons = FXCollections.observableArrayList();
+        private final ObservableList<Admin> admins = FXCollections.observableArrayList();
 
         AddressBookStub(Collection<Person> persons) {
             this.persons.setAll(persons);
@@ -105,6 +107,11 @@ public class AddressBookTest {
         @Override
         public ObservableList<Person> getPersonList() {
             return persons;
+        }
+
+        @Override
+        public ObservableList<Admin> getAdminList() {
+            return admins;
         }
 
         //TODO havent integrated this code properly yet
