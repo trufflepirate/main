9641d47bdf23efbd8723141e9043fc44f1fbe004
futia <ftygary@gmail.com>
2018-10-14 21:31:49 +0800

    travis errors

--- src/main/java/seedu/address/model/job/JobPriority.java
+++ src/main/java/seedu/address/model/job/JobPriority.java
@@ -12,9 +12,12 @@ public enum JobPriority {
     public static final String MESSAGE_JOBPRIORITY_CONSTRAINTS =
             "Job priority should only be specified as HIGH, MEDIUM or LOW";
 
+    /**
+     *checks if the input priority is a acceptable priority for the job
+     */
     public static boolean isValidPriority(JobPriority priority) {
         boolean isValid = true;
-        if(priority != HIGH || priority != MEDIUM || priority != LOW) {
+        if (priority != HIGH || priority != MEDIUM || priority != LOW) {
             isValid = false;
         }
         return isValid;
