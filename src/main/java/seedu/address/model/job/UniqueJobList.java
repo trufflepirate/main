b0bbb6f85c74874974c69f5bb2879713f8ca1271
Teo Jun Jie <e0148239@u.nus.edu>
2018-10-23 16:24:57 +0800

    added some stuff to uniquejoblist

--- src/main/java/seedu/address/model/job/UniqueJobList.java
+++ src/main/java/seedu/address/model/job/UniqueJobList.java
@@ -43,7 +43,7 @@ public class UniqueJobList {
             throw new DuplicateJobException();
         }
         internalList.add(toAdd);
-        sortJobs(internalList);
+        sortJobs(internalList );
     }
 
     /**
