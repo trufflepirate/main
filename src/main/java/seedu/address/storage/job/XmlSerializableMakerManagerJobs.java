5af0a6d2a29b6fdeb5ec3a5de305c56945aa622a
teojunjie <e0148239@u.nus.edu>
2018-10-30 17:15:30 +0800

    Resolving travis errors

--- src/main/java/seedu/address/storage/job/XmlSerializableMakerManagerJobs.java
+++ src/main/java/seedu/address/storage/job/XmlSerializableMakerManagerJobs.java
@@ -2,13 +2,11 @@ package seedu.address.storage.job;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
-import seedu.address.commons.core.LogsCenter;
 import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.model.AddressBook;
 import seedu.address.model.ReadOnlyAddressBook;
--- src/main/java/seedu/address/storage/machine/XmlSerializableMakerManagerMachines.java
+++ src/main/java/seedu/address/storage/machine/XmlSerializableMakerManagerMachines.java
@@ -2,12 +2,10 @@ package seedu.address.storage.machine;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Logger;
 import java.util.stream.Collectors;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
-import seedu.address.commons.core.LogsCenter;
 import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.model.AddressBook;
 import seedu.address.model.ReadOnlyAddressBook;
