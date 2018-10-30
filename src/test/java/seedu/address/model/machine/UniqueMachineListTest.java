27ce27905e3653290f7d7dbbbb2c08130a2f0368
teojunjie <e0148239@u.nus.edu>
2018-10-31 02:28:11 +0800

    Updated user guide and wrote test for uniquemachinelist

--- src/test/java/seedu/address/model/machine/UniqueMachineListTest.java
+++ src/test/java/seedu/address/model/machine/UniqueMachineListTest.java
@@ -7,6 +7,8 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
+import seedu.address.model.machine.exceptions.DuplicateMachineException;
+import seedu.address.model.machine.exceptions.MachineNotFoundException;
 import seedu.address.testutil.testdata.ValidMachines;
 
 public class UniqueMachineListTest {
@@ -31,6 +33,50 @@ public class UniqueMachineListTest {
         uniqueMachineList.add(ValidMachines.JJPRINTER);
         assertTrue(uniqueMachineList.contains(ValidMachines.JJPRINTER));
     }
+
+    @Test
+    public void add_nullMachine_throwsNullPointerException() {
+        thrown.expect(NullPointerException.class);
+        uniqueMachineList.add(null);
+    }
+
+    @Test
+    public void add_duplicateMachine_throwsDuplicateMachineException() {
+        uniqueMachineList.add(ValidMachines.JJPRINTER);
+        thrown.expect(DuplicateMachineException.class);
+        uniqueMachineList.add(ValidMachines.JJPRINTER);
+    }
+
+    @Test
+    public void setMachine_nullTargetMachine_throwsNullPointerException() {
+        thrown.expect(NullPointerException.class);
+        uniqueMachineList.setMachine(null, ValidMachines.JJPRINTER);
+    }
+
+    @Test
+    public void setMachine_nullEditedMachine_throwsNullPointerException() {
+        thrown.expect(NullPointerException.class);
+        uniqueMachineList.setMachine(ValidMachines.JJPRINTER, null);
+    }
+
+    @Test
+    public void remove_nullMachine_throwsNullPointerException() {
+        thrown.expect(NullPointerException.class);
+        uniqueMachineList.remove(null);
+    }
+
+    @Test
+    public void remove_machineDoesNotExist_throwsMachineNotFoundException() {
+        thrown.expect(MachineNotFoundException.class);
+        uniqueMachineList.remove(ValidMachines.JJPRINTER);
+    }
+    @Test
+    public void asUnmodifiableObservableList_modifyList_throwsUnsupportedOperation() {
+        thrown.expect(UnsupportedOperationException.class);
+        uniqueMachineList.asUnmodifiableObservableList().remove(0);
+    }
+
+
 
 
 }
