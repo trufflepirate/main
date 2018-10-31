ab75674598f0a4785f170a3a2fcf1bcc29db1109
Ftywan <ftygary@gmail.com>
2018-10-31 23:29:59 +0800

    fix travis errors

--- src/test/java/seedu/address/model/job/UniqueJobListTest.java
+++ src/test/java/seedu/address/model/job/UniqueJobListTest.java
@@ -1,6 +1,5 @@
 package seedu.address.model.job;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertTrue;
