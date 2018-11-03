c0ee2fe57d397a477136181c114d2ee5e58fddb2
teojunjie <e0148239@u.nus.edu>
2018-11-03 18:09:30 +0800

    Resolving travis erros

--- src/main/java/seedu/address/logic/commands/machine/EditMachineCommand.java
+++ src/main/java/seedu/address/logic/commands/machine/EditMachineCommand.java
@@ -12,9 +12,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
-import java.util.logging.Logger;
 
-import seedu.address.commons.core.LogsCenter;
 import seedu.address.commons.util.CollectionUtil;
 import seedu.address.logic.CommandHistory;
 import seedu.address.logic.commands.Command;
@@ -49,7 +47,6 @@ public class EditMachineCommand extends Command {
     private final MachineName machineName;
     private final EditMachineDescriptor editMachineDescriptor;
 
-    private final Logger logger = LogsCenter.getLogger(EditMachineCommand.class);
 
     /**
      * @param machineName           of the person in the filtered person list to edit
@@ -77,9 +74,6 @@ public class EditMachineCommand extends Command {
         }
 
         Machine editedMachine = createEditedMachine(machineToEdit, editMachineDescriptor);
-
-        logger.info("Is same machine : " + ((machineToEdit.isSameMachine(editedMachine)) ? "True" : "False"));
-        logger.info("Model has machine : " + (model.hasMachine(editedMachine) ? "True" : "False"));
         if (!machineToEdit.isSameMachine(editedMachine) && model.hasMachine(editedMachine)) {
             throw new CommandException(MESSAGE_DUPLICATE_MACHINE);
         }
