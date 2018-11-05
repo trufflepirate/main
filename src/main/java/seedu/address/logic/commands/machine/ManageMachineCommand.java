df28d7b7752d2840cf2709808d671733e063d5de
teojunjie <e0148239@u.nus.edu>
2018-11-05 16:00:04 +0800

    Added prompt for user to determine if he wants to execute the flush

--- src/main/java/seedu/address/logic/commands/machine/ManageMachineCommand.java
+++ src/main/java/seedu/address/logic/commands/machine/ManageMachineCommand.java
@@ -5,6 +5,10 @@ import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
 import static seedu.address.model.Model.PREDICATE_SHOW_ALL_JOBS;
 import static seedu.address.model.Model.PREDICATE_SHOW_ALL_MACHINES;
 
+import java.util.Optional;
+
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import seedu.address.logic.CommandHistory;
 import seedu.address.logic.commands.Command;
 import seedu.address.logic.commands.CommandResult;
@@ -35,6 +39,7 @@ public class ManageMachineCommand extends Command {
     public static final String MESSAGE_MACHINE_STILL_HAVE_UNFINISHED_JOBS = "Machine still have unfinished jobs";
     public static final String MESSAGE_MACHINE_DOES_NOT_HAVE_CLEANABLE_JOBS = "Machine is already clean";
     public static final String MESSAGE_NO_SUCH_MANAGE_MACHINE_COMMAND = "No such option to manage machine";
+    public static final String MESSAGE_MACHINE_NOT_FLUSHED = "Machine is not flushed";
     private static final String MESSAGE_ACCESS_DENIED =
         "Non admin user is not allowed to manage a machine from maker manager";
 
@@ -71,11 +76,24 @@ public class ManageMachineCommand extends Command {
                 model.updateFilteredMachineList(PREDICATE_SHOW_ALL_MACHINES);
                 return new CommandResult(String.format(MESSAGE_REMOVE_MACHINE_SUCCESS, machineToManage));
             case OPTION_FLUSH:
-                model.flushMachine(machineToManage);
-                model.commitAddressBook();
-                model.updateFilteredMachineList(PREDICATE_SHOW_ALL_MACHINES);
-                model.updateFilteredJobListInAllMachines(PREDICATE_SHOW_ALL_JOBS);
-                return new CommandResult(String.format(MESSAGE_FLUSH_MACHINE_SUCCESS, machineToManage));
+                Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+                alert.setTitle("Require confirmation");
+                alert.setHeaderText("Are you sure you want to flush " + machineToManage.getName().fullName + "?");
+                alert.setContentText("Are you ok with this?" + "\nAlternatively, you can click cancel and proceed to move "
+                    + "all the jobs in the machine to another machine"
+                    + "\nOtherwise, all jobs in the machine will be removed");
+
+                Optional<ButtonType> result = alert.showAndWait();
+                if (result.get() == ButtonType.OK){
+                    model.flushMachine(machineToManage);
+                    model.commitAddressBook();
+                    model.updateFilteredMachineList(PREDICATE_SHOW_ALL_MACHINES);
+                    model.updateFilteredJobListInAllMachines(PREDICATE_SHOW_ALL_JOBS);
+                    return new CommandResult(String.format(MESSAGE_FLUSH_MACHINE_SUCCESS, machineToManage));
+                } else {
+                    return new CommandResult(String.format(MESSAGE_MACHINE_NOT_FLUSHED, machineToManage));
+                }
+
             case OPTION_CLEAN:
                 if (machineToManage.hasCleanableJobs()) {
                     model.cleanMachine(machineToManage);
